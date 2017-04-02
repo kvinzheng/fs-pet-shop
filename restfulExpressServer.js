3a30d23dc5c9299e11893edd77041fa3ae9092d1
Kevin(Jiahong) Zheng <kvinzheng@gmail.com>
2017-04-02 11:04:00 -0700

    finished some changes

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -9,7 +9,7 @@ let port = process.env.PORT || 6004;
 
 app.disable('x-powered-by');
 app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(bodyParser.urlencoded({ extended: false })); // support encoded bodies
 
 app.use(morgan('dev'));
 
